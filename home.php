8315d9c00f055806a115db1aaee693cd1e217612
duangkamol130262-sketch <duangkamol130262@gmail.com>
2025-12-26 10:15:45 +0700

    Update home.php

--- home.php
+++ home.php
@@ -23,8 +23,8 @@
 			<td width="190" bgcolor="#FFFF66"><ul>
 				<li><a href="home.php">ประตูบ้าน ahahahahahaha</li>
 				<li><a href="room.php">ห้องพัก&ราคา</li>
-				<li><a herf="guest.php">สมุดเยี่ยม</li>
-				<li><a href="contact.php">ติดต่ออออออออออออออออออออออออออ</li>
+				<li><a href="guest.php">สมุดเยี่ยม</li>
+				<li><a href="contact.php">ติดต่อ</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
